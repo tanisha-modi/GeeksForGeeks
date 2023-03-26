e2734602607cbb52924c7d14853d7a050c9894a3
tanisha <tanishamodi12@gmail.com>
2023-03-26 20:11:29 +0530

    Q21

---
+++ Q21_Pattern19.java
@@ -0,0 +1,42 @@
+// link to the question --> https://practice.geeksforgeeks.org/problems/double-triangle-pattern/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_19
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=1; i <= n; i++)
+        {
+            for(int j = 1; j <= 2*n; j++)
+            {
+                if(j <= n-i+1 || j > n+i-1 )
+                {
+                   System.out.print("*");
+                }
+                else{
+                   System.out.print(" ");
+                     
+                }
+            }
+            
+            System.out.println("");
+        }
+        for(int i=1; i <= n; i++)
+        {
+            for(int j = 1; j <= 2*(n) ; j++)
+            {
+                if(j <= i){
+                   System.out.print("*");
+                    
+                }
+                else if(j > n*2-i)
+                {
+                   System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            
+            System.out.println("");
+        }
+    }
+}
